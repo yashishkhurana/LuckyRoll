577968c6594bbc0ccf851d01fe3469c60332d891
Yashish Khurana <62921042+yashishkhurana@users.noreply.github.com>
2021-03-31 17:15:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var randomNumber1 = Math.ceil(Math.random()*6);
+var randomDiceImage = "images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage);
+
+var randomNumber2 = Math.ceil(Math.random()*6);
+var randomDiceImage2 = "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+}
+else if(randomNumber1===randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Its a Draw!";
+}
